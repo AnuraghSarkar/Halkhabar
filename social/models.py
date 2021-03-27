a46d54593629519e1c2369d41c5f24d43c2466fa
AnuraghSarkar <trithatimalsina@gmail.com>
2021-03-27 10:55:26 +0545

    Adding Following Feature

--- social/models.py
+++ social/models.py
@@ -21,7 +21,8 @@ class UserProfile(models.Model):
   bio = models.TextField(max_length=500, blank=True, null=True)
   birthdate = models.DateField(null=True, blank=True)
   location = models.CharField(max_length=100, blank=True, null=True)
-  picture = models.ImageField(upload_to='uploads/profile_pictures', default='upload/profile_pictures/ok.png', blank=True)
+  picture = models.ImageField(upload_to='upload/profile_pictures', default='upload/profile_pictures/ok.png', blank=True)
+  followers = models.ManyToManyField(User, blank=True, related_name='followers')
   
 
 @receiver(post_save, sender=User)
