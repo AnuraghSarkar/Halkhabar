94e2a866dcd38326b3af7fa821e9d675ef2e7ce2
AnuraghSarkar <trithatimalsina@gmail.com>
2021-03-29 15:29:54 +0545

    User Search Functionality Added

--- social/urls.py
+++ social/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.urls.conf import include
-from .views import PostListView, PostDetailView, PostEditView, PostDeleteView, CommentDeleteView, ProfileView, ProfileEditView, AddFollower, RemoveFollower, AddDislike, AddLike
+from .views import PostListView, PostDetailView, PostEditView, PostDeleteView, CommentDeleteView, ProfileView, ProfileEditView, AddFollower, RemoveFollower, AddDislike, AddLike, UserSearch
 
 urlpatterns = [
     path('', PostListView.as_view(), name='post_list'),
@@ -16,6 +16,6 @@ urlpatterns = [
     path('profile/edit/<int:pk>/',ProfileEditView.as_view(), name='profile_edit'),
     path('profile/<int:pk>/followers/add', AddFollower.as_view(), name='add_follower'),    
     path('profile/<int:pk>/followers/remove', RemoveFollower.as_view(), name='remove_follower'),
-
+    path('search/', UserSearch.as_view(), name='profile_search')
 
 ]
--- social/views.py
+++ social/views.py
@@ -238,11 +238,13 @@ class AddDislike(LoginRequiredMixin, View):
 
 class UserSearch(View):
     def get(self, request, *args, **kwargs):
-        query = self.request.get('query')
+        query = self.request.GET.get('query')
         profile_list = UserProfile.objects.filter(
             Q(user__username__icontains=query)
         )
+
         context = {
             'profile_list': profile_list,
         }
+
         return render(request, 'social/search.html', context)
